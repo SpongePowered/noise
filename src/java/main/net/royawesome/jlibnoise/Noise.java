2eda36da0205d9711a6c7ed289afc05784beeb5c
RoyAwesome <madKylegar@gmail.com>
2012-01-05 01:14:22 -0800

    Formatting

--- src/java/main/net/royawesome/jlibnoise/Noise.java
+++ src/java/main/net/royawesome/jlibnoise/Noise.java
@@ -102,60 +102,60 @@ public class Noise {
 		return (n * (n * n * 60493 + 19990303) + 1376312589) & 0x7fffffff;
 
 	}
-	
+
 	public static double ValueCoherentNoise3D(double x, double y, double z, int seed, NoiseQuality quality){
-		 // Create a unit-length cube aligned along an integer boundary.  This cube
-		  // surrounds the input point.
-		  int x0 = (x > 0.0? (int)x: (int)x - 1);
-		  int x1 = x0 + 1;
-		  int y0 = (y > 0.0? (int)y: (int)y - 1);
-		  int y1 = y0 + 1;
-		  int z0 = (z > 0.0? (int)z: (int)z - 1);
-		  int z1 = z0 + 1;
+		// Create a unit-length cube aligned along an integer boundary.  This cube
+		// surrounds the input point.
+		int x0 = (x > 0.0? (int)x: (int)x - 1);
+		int x1 = x0 + 1;
+		int y0 = (y > 0.0? (int)y: (int)y - 1);
+		int y1 = y0 + 1;
+		int z0 = (z > 0.0? (int)z: (int)z - 1);
+		int z1 = z0 + 1;
 
-		  // Map the difference between the coordinates of the input value and the
-		  // coordinates of the cube's outer-lower-left vertex onto an S-curve.
-		  double xs = 0, ys = 0, zs = 0;
-		  if(quality == NoiseQuality.FAST){
-				xs = (x - (double)x0);
-				ys = (y - (double)y0);
-				zs = (z - (double)z0);
-			}else if( quality == NoiseQuality.STANDARD){
-				xs = Utils.SCurve3 (x - (double)x0);
-				ys = Utils.SCurve3 (y - (double)y0);
-				zs = Utils.SCurve3 (z - (double)z0);
-			}else{
+		// Map the difference between the coordinates of the input value and the
+		// coordinates of the cube's outer-lower-left vertex onto an S-curve.
+		double xs = 0, ys = 0, zs = 0;
+		if(quality == NoiseQuality.FAST){
+			xs = (x - (double)x0);
+			ys = (y - (double)y0);
+			zs = (z - (double)z0);
+		}else if( quality == NoiseQuality.STANDARD){
+			xs = Utils.SCurve3 (x - (double)x0);
+			ys = Utils.SCurve3 (y - (double)y0);
+			zs = Utils.SCurve3 (z - (double)z0);
+		}else{
 
-				xs = Utils.SCurve5 (x - (double)x0);
-				ys = Utils.SCurve5 (y - (double)y0);
-				zs = Utils.SCurve5 (z - (double)z0);
+			xs = Utils.SCurve5 (x - (double)x0);
+			ys = Utils.SCurve5 (y - (double)y0);
+			zs = Utils.SCurve5 (z - (double)z0);
 
-			}
+		}
 
 
-		  // Now calculate the noise values at each vertex of the cube.  To generate
-		  // the coherent-noise value at the input point, interpolate these eight
-		  // noise values using the S-curve value as the interpolant (trilinear
-		  // interpolation.)
-		  double n0, n1, ix0, ix1, iy0, iy1;
-		  n0   = ValueNoise3D (x0, y0, z0, seed);
-		  n1   = ValueNoise3D (x1, y0, z0, seed);
-		  ix0  = Utils.LinearInterp (n0, n1, xs);
-		  n0   = ValueNoise3D (x0, y1, z0, seed);
-		  n1   = ValueNoise3D (x1, y1, z0, seed);
-		  ix1  = Utils.LinearInterp (n0, n1, xs);
-		  iy0  = Utils.LinearInterp (ix0, ix1, ys);
-		  n0   = ValueNoise3D (x0, y0, z1, seed);
-		  n1   = ValueNoise3D (x1, y0, z1, seed);
-		  ix0  = Utils.LinearInterp (n0, n1, xs);
-		  n0   = ValueNoise3D (x0, y1, z1, seed);
-		  n1   = ValueNoise3D (x1, y1, z1, seed);
-		  ix1  = Utils.LinearInterp (n0, n1, xs);
-		  iy1  = Utils.LinearInterp (ix0, ix1, ys);
-		  return Utils.LinearInterp (iy0, iy1, zs);
+		// Now calculate the noise values at each vertex of the cube.  To generate
+		// the coherent-noise value at the input point, interpolate these eight
+		// noise values using the S-curve value as the interpolant (trilinear
+		// interpolation.)
+		double n0, n1, ix0, ix1, iy0, iy1;
+		n0   = ValueNoise3D (x0, y0, z0, seed);
+		n1   = ValueNoise3D (x1, y0, z0, seed);
+		ix0  = Utils.LinearInterp (n0, n1, xs);
+		n0   = ValueNoise3D (x0, y1, z0, seed);
+		n1   = ValueNoise3D (x1, y1, z0, seed);
+		ix1  = Utils.LinearInterp (n0, n1, xs);
+		iy0  = Utils.LinearInterp (ix0, ix1, ys);
+		n0   = ValueNoise3D (x0, y0, z1, seed);
+		n1   = ValueNoise3D (x1, y0, z1, seed);
+		ix0  = Utils.LinearInterp (n0, n1, xs);
+		n0   = ValueNoise3D (x0, y1, z1, seed);
+		n1   = ValueNoise3D (x1, y1, z1, seed);
+		ix1  = Utils.LinearInterp (n0, n1, xs);
+		iy1  = Utils.LinearInterp (ix0, ix1, ys);
+		return Utils.LinearInterp (iy0, iy1, zs);
 
 	}
-	
+
 	public static double ValueNoise3D(int x, int y, int z, int seed){
 		return 1.0 - ((double)IntValueNoise3D (x, y, z, seed) / 1073741824.0);
 
