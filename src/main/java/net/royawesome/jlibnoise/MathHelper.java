369756419f135ca1da139330878dcc95b5ca0774
RoyAwesome <madKylegar@gmail.com>
2012-07-03 18:28:42 -0700

    Updated header on MathHelper

--- src/main/java/net/royawesome/jlibnoise/MathHelper.java
+++ src/main/java/net/royawesome/jlibnoise/MathHelper.java
@@ -1,28 +1,21 @@
-/*
- * This file is part of SpoutAPI.
- *
- * Copyright (c) 2011-2012, SpoutDev <http://www.spout.org/>
- * SpoutAPI is licensed under the SpoutDev License Version 1.
- *
- * SpoutAPI is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * In addition, 180 days after any changes are published, you can use the
- * software, incorporating those changes, under the terms of the MIT license,
- * as described in the SpoutDev License Version 1.
- *
- * SpoutAPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License,
- * the MIT license and the SpoutDev License Version 1 along with this program.
- * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
- * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
- * including the MIT license.
+/* Copyright (C) 2011 Garrett Fleenor
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3.0 of the License, or (at
+ your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ License (COPYING.txt) for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ This file relicensed with permission from the Spout Project (www.github.com/SpoutDev)
+
  */
 package net.royawesome.jlibnoise;
 
