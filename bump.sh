b113a66aee936da6a82de178b608c8e18ed49382
Luke Spragg <luke@spragg.me>
2014-12-15 21:58:45 -0500

    Add script for bumping version for releases

---
+++ bump.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function die_with() { echo "$*" >&2; exit 1; }
+
+echo "Getting current version from pom.xml"
+CURRENT_VERSION="`sed -n 's|.*<version>\(.*\)</version>.*|\1|p' pom.xml | awk '{ print $1; exit }'`"  && echo "Current version from pom.xml: $CURRENT_VERSION"
+
+read -p "New version: " NEW_VERSION || die_with "Prompt for new version failed"
+
+if ! echo $NEW_VERSION | grep -i -- '-SNAPSHOT' >/dev/null; then echo "WARNING: changing to a release version!"; fi
+
+echo "Updating the project version in build.gradle, pom.xml and README.md to $NEW_VERSION"
+sed -ri "s/"`echo $CURRENT_VERSION | sed 's/\./\\\\./g'`"/$NEW_VERSION/g" build.gradle pom.xml README.md || die_with "Failed to update the project version!"
